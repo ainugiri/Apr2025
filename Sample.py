537a86a303430725dfae16d95af732ac597b05c2
ainugiri <40005294+ainugiri@users.noreply.github.com>
2025-04-23 15:26:06 +0530

    Add files via upload

---
+++ Sample.py
@@ -0,0 +1,5 @@
+print("Hello, World!")
+print("This is a sample Python script.")
+print("It demonstrates how to create a simple script.")
+print("You can run this script in any Python environment.")
+print("Make sure you have Python installed on your system.")
