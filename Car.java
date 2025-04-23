6ef88436997d1eeef53d0857199649e146b4f1e4
Giri Prasad P <giriprasa@gmail.com>
2025-04-23 15:32:36 +0530

    New Update By Giri

--- Car.java
+++ Car.java
@@ -1,8 +1,9 @@
 class Car{
+    public void display(){
+        System.out.println("This is a car class");
+    }   
     public static void main(String []args){
-        System.out.println("Car Class");
-        System.out.println("New commit by the user");
-        System.out.println("Jenkins checks for the newer commits/ change in the code");
-        System.out.println("Jenkins pull the code, start executing w.r.t");
+        Car c1 = new Car();
+        c1.display();
     }
 }
--- Sample.py
+++ Sample.py
@@ -1,6 +1,13 @@
-print("Hello, World!")
-print("This is a sample Python script.")
-print("It demonstrates how to create a simple script.")
-print("You can run this script in any Python environment.")
-print("Make sure you have Python installed on your system.")
-print("Thank You!")
+def welcome():
+    print("Hello, World!")
+    print("This is a sample Python script.")
+    print("It demonstrates how to create a simple script.")
+    print("You can run this script in any Python environment.")
+    print("Make sure you have Python installed on your system.")
+    print("Thank You!")
+    
+
+welcome()
+welcome()
+
+
