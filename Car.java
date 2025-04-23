3583d24781271b3adcf30c3d139dae100a1c0994
ainugiri <40005294+ainugiri@users.noreply.github.com>
2025-04-23 15:12:14 +0530

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,17 @@
+class Car{
+    public static void main(String []args){
+        System.out.println("Car Class");
+        System.out.println("Car Class");
+        System.out.println("Car Class");
+        System.out.println("Car Class");
+        System.out.println("Car Class");
+        System.out.println("Car Class");
+        System.out.println("Car Class");
+        System.out.println("Car Class");
+        System.out.println("Car Class");
+        System.out.println("Car Class");
+        System.out.println("Car Class");
+        System.out.println("Car Class");
+        System.out.println("Car Class");
+    }
+}
