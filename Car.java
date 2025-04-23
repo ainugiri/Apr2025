3626dc5e164b7cbb350e28fa78e493efe2195bdf
ainugiri <40005294+ainugiri@users.noreply.github.com>
2025-04-23 15:23:53 +0530

    Update3 Car.java

--- Car.java
+++ Car.java
@@ -1,17 +1,8 @@
 class Car{
     public static void main(String []args){
         System.out.println("Car Class");
-        System.out.println("Car Class");
-        System.out.println("Car Class");
-        System.out.println("Car Class");
-        System.out.println("Car Class");
-        System.out.println("Car Class");
-        System.out.println("Car Class");
-        System.out.println("Car Class");
-        System.out.println("Car Class");
-        System.out.println("Car Class");
-        System.out.println("Car Class");
-        System.out.println("Car Class");
-        System.out.println("Car Class");
+        System.out.println("New commit by the user");
+        System.out.println("Jenkins checks for the newer commits/ change in the code");
+        System.out.println("Jenkins pull the code, start executing w.r.t");
     }
 }
